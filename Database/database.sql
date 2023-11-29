6eec020c49d55213bff93fc6cab6b7c9dcc7266e
adarshdebata <adarshdebata00@gmail.com>
2023-11-29 21:45:57 +0530

    DB Update

--- Database/database.sql
+++ Database/database.sql
@@ -1,43 +1,40 @@
--- Create User Table
+
 CREATE TABLE UserTable (
     user_uuid UUID PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255),
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(15) NOT NULL,
+    middle_name VARCHAR(15),
+    last_name VARCHAR(15) NOT NULL,
     dob DATE NOT NULL,
     bloodgroup VARCHAR(10) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     phone_number VARCHAR(15) NOT NULL
 );
 
--- Create Employee Type Table
 CREATE TABLE EmployeeType (
     emp_type_id SERIAL PRIMARY KEY NOT NULL,
-    employee_type VARCHAR(10) NOT NULL
+    employee_type VARCHAR(20) NOT NULL
 );
 
--- Create Company Table
 CREATE TABLE Company (
     company_uuid UUID PRIMARY KEY NOT NULL,
-    company_id VARCHAR(8) NOT NULL,
-    company_name VARCHAR(255) NOT NULL,
+    company_id VARCHAR(10) NOT NULL,
+    company_name VARCHAR(60) NOT NULL,
     company_admin UUID,
     description TEXT,
     contact_no VARCHAR(15) NOT NULL,
-    email_id VARCHAR(255) NOT NULL,
-    address_lane VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    country VARCHAR(255) NOT NULL,
+    email_id VARCHAR(50) NOT NULL,
+    address_lane VARCHAR(70) NOT NULL,
+    city VARCHAR(25) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    country VARCHAR(20) NOT NULL,
     pincode VARCHAR(10) NOT NULL,
     created_at TIMESTAMP
 );
 
--- Create Department Table
 CREATE TABLE Department (
     dept_uuid UUID PRIMARY KEY NOT NULL,
-    dept_id VARCHAR(8) NOT NULL,
-    dept_name VARCHAR(255) NOT NULL,
+    dept_id VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(50) NOT NULL,
     description TEXT,
     company_uuid UUID NOT NULL,
     dept_head UUID,
@@ -46,10 +43,9 @@ CREATE TABLE Department (
     FOREIGN KEY (dept_head) REFERENCES UserTable(user_uuid)
 );
 
--- Create Employee Table
 CREATE TABLE Employee (
     employee_uuid UUID PRIMARY KEY NOT NULL,
-    employee_id VARCHAR(8) NOT NULL,
+    employee_id VARCHAR(10) NOT NULL,
     user_uuid UUID NOT NULL,
     company_uuid UUID NOT NULL,
     department_uuid UUID NOT NULL,
