6e3ca84a48b2389510aff8549682aa7101180199
adarshdebata <adarshdebata00@gmail.com>
2023-12-04 18:21:54 +0530

    project update

--- api-modules/userRegistrationApi.js
+++ api-modules/userRegistrationApi.js
@@ -1,70 +1,36 @@
+
 const express = require('express');
 const router = express.Router();
-const { Pool } = require('pg');
+const uuid = require('uuid');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+const users = [];
 
 
-const validateUserRegistrationInput = (data) => {
-    
+const validateUserInput = (data) => {
+    const { first_name, last_name, dob, bloodgroup, email, phone_number } = data;
+    const isValidDate = !isNaN(Date.parse(dob));
+
     return (
-        data.first_name && typeof data.first_name === 'string' &&
-        data.last_name && typeof data.last_name === 'string' &&
-        data.dob && data.dob instanceof Date &&
-        data.bloodgroup && typeof data.bloodgroup === 'string' &&
-        data.email && typeof data.email === 'string' &&
-        data.phone_number && typeof data.phone_number === 'string'
+        first_name && last_name && isValidDate && bloodgroup && email && phone_number &&
+        typeof first_name === 'string' &&
+        typeof last_name === 'string' &&
+        typeof bloodgroup === 'string' &&
+        typeof email === 'string' &&
+        typeof phone_number === 'string'
     );
-};
+}
 
-
-router.post('/register', async (req, res) => {
-    try {
-        const userData = req.body;
-
-        if (!validateUserRegistrationInput(userData)) {
-            return res.status(400).json({ error: 'Invalid user registration input' });
-        }
-
-        const registerUserQuery = `INSERT INTO usertable (
-                user_uuid,
-                first_name,
-                middle_name,
-                last_name,
-                dob,
-                bloodgroup,
-                email,
-                phone_number,
-                created_at
-            ) VALUES (
-                uuid_generate_v4(),
-                $1, $2, $3, $4, $5, $6, $7, NOW()
-            ) RETURNING *`;
-
-        const values = [
-            userData.first_name,
-            userData.middle_name,
-            userData.last_name,
-            userData.dob,
-            userData.bloodgroup,
-            userData.email,
-            userData.phone_number,
-        ];
-
-        const response = await pool.query(registerUserQuery, values);
-        const registeredUser = response.rows[0];
-
-        res.json(registeredUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+router.post('/register', (req, res) => {
+    const userData = req.body;
+    if (!validateUserInput(userData)) {
+        return res.status(400).json({ error: 'Invalid user input' });
     }
+
+
+    const user_uuid = uuid.v4();
+    users.push({ user_uuid, ...userData });
+
+    res.json({ user_uuid, ...userData });
 });
 
 module.exports = router;
