1aed032866b060f7081d457a0e96f5e2cbf883d2
adarshdebata <adarshdebata00@gmail.com>
2023-12-04 11:59:51 +0530

    Project_Update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 //const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000 ;
@@ -6,3 +7,4 @@ const port = process.env.PORT || 5000 ;
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
 });
+
